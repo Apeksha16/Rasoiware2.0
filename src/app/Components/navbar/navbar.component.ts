7b05cb74a4240c6320b821d701f3ed41dc15065f
Pranav Katiyar <pk.pranavkatiyar@gmail.com>
2023-09-19 01:20:25 +0530

    dropdown fixeS

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -3,37 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-config:any={
-  logoPath:'assets/rasoiware.png',
-  links:[
-    {
-      name:'Home',
-      path:'',
-    },
-    {
-      name:'Shop',
-      path:'',
-    },
-    {
-      name:'Product',
-      path:'',
-    },
-    {
-      name:'Blog',
-      path:'',
-    },
-    {
-      name:'About',
-      path:'',
-    },
-    {
-      name:'Contact',
-      path:'',
-    },
-  ],
-}
+  config: any = {
+    logoPath: 'assets/rasoiware.png',
+    links: [
+      {
+        name: 'Home',
+        path: '',
+        isShow: false,
+      },
+      {
+        name: 'Shop',
+        path: '',
+        isShow: false,
+      },
+      {
+        name: 'Product',
+        path: '',
+        isShow: false,
+      },
+      {
+        name: 'Blog',
+        path: '',
+        isShow: false,
+      },
+      {
+        name: 'About',
+        path: '',
+        isShow: false,
+      },
+      {
+        name: 'Contact',
+        path: '',
+        isShow: false,
+      },
+    ],
+  };
 
+  // isShow: boolean = false;
+
+  onMouseOver(item: string, i: number) {
+    if (item === 'Shop' || item === 'Product' || item === 'Home') {
+      this.config.links[i].isShow = true;
+    }
+  }
 }
